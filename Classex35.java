3c870b99edf7cdc849e28f420326b2120bc9cd27
Nathnael Atsbeha <Natitad0989@gmaol.com>
2015-10-01 13:38:22 -0700

    Add file

---
+++ Classex35.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Classex35 {
+
+	public static void main(String[] args) {
+		System.out.println("Enter role");
+		Scanner scan = new Scanner(System.in);
+		String Role = scan.nextLine();
+		switch(Role){
+			case "Administrator":
+			System.out.println("Administrator");
+			break;
+
+			case "Faculity":
+			System.out.println("Administrator");
+			break;
+
+			case "Stuff":
+			System.out.println("Administrator");
+			break;
+
+			case "Student":
+			System.out.println("Administrator");
+			break;
+
+			case "Guest":
+			System.out.println("Administrator");
+			break;
+		}
+	}
+
+}
